9a0f648551668ca8b9293cd9bffa8b577851a514
Aman-kumar45 <amanthakur03032005.com@gmail.com>
2026-09-07 16:48:37 +0530

    Add initial test cases for home and add endpoints

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,17 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Hello World"
+    }
+    
+def test_add():
+    response = client.get('/add/10/20')
+    assert response.status_code == 200
+    assert response.json() == {
+        "result" : 30
+    }
