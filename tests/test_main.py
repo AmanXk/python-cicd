e9ed816f504f535f9dbab9817d1fe1b03b1181e7
Aman-kumar45 <amanthakur03032005.com@gmail.com>
2026-09-08 13:43:02 +0530

    Fix formatting in test_main.py by removing unnecessary blank lines

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,10 +10,11 @@ def test_home():
     assert response.json() == {
         "message": "Hello World"
     }
-    
+
 def test_add():
     response = client.get('/add/10/20')
     assert response.status_code == 200
     assert response.json() == {
         "result" : 30
     }
+    
